970a78121362f3ccefb25f1280c938a4730cf9c3
Chris Bornholdt <chris.bornholdt@gmail.com>
2018-09-09 10:19:20 -0400

    Test of tab/space fix

--- Source/LordJobs/PartyJob_RecRoom.cs
+++ Source/LordJobs/PartyJob_RecRoom.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
@@ -10,12 +10,12 @@ using Harmony;
 
 namespace EnhancedParty
 {
-	public class PartyJob_RecRoom : EnhancedLordJob_Party
-	{
-		protected RecRoomParty_PrepareToil prepareToil;
-		protected RecRoomParty_PartyToil partyToil;
+    public class PartyJob_RecRoom : EnhancedLordJob_Party
+    {
+        protected RecRoomParty_PrepareToil prepareToil;
+        protected RecRoomParty_PartyToil partyToil;
 
-		public PartyJob_RecRoom(EnhancedPartyDef def, Pawn organizer, IntVec3 spot) : base(def, organizer, spot) 
+        public PartyJob_RecRoom(EnhancedPartyDef def, Pawn organizer, IntVec3 spot) : base(def, organizer, spot) 
         {
             prepareToil = new RecRoomParty_PrepareToil();
             partyToil = new RecRoomParty_PartyToil();
@@ -27,53 +27,53 @@ namespace EnhancedParty
             partyToil = new RecRoomParty_PartyToil();
         }
 
-		public override bool AllowStartNewGatherings => base.AllowStartNewGatherings;
+        public override bool AllowStartNewGatherings => base.AllowStartNewGatherings;
 
-		protected override EnhancedLordToil_Party PartyToil => partyToil;
+        protected override EnhancedLordToil_Party PartyToil => partyToil;
 
-		protected override EnhancedLordToil_PrepareParty PrepareToil => prepareToil;
+        protected override EnhancedLordToil_PrepareParty PrepareToil => prepareToil;
 
-		public override bool IsAttendingParty(Pawn pawn)
-		{
-			return base.IsAttendingParty(pawn);
-		}
+        public override bool IsAttendingParty(Pawn pawn)
+        {
+            return base.IsAttendingParty(pawn);
+        }
 
-		public override bool AllowRolelessPawnsToReplenish => true;
+        public override bool AllowRolelessPawnsToReplenish => true;
 
-		public int TotalSnacksNeeded() => prepareToil.GetDesiredSnackCount();
+        public int TotalSnacksNeeded() => prepareToil.GetDesiredSnackCount();
 
-		public int SnacksAlreadySetup() => prepareToil.GetSetupSnackCount(); 
+        public int SnacksAlreadySetup() => prepareToil.GetSetupSnackCount(); 
 
-		protected override void CreatePartyRoles()
-		{
-			var partyGoers = new LordPawnRole("PartyGoers", this) {
-				pawnValidator = (Pawn p) => true,
-				pawnReplenishPriority = (Pawn p) => 1f,
-				replenishCompleter = (List<Pawn> pawns) => false    //hungry, will default to everything
-			};
-			partyGoers.Configure(enabled: true, priority: 1, reassignableFrom: true
-				, seekReplacements: false, seekReplenishment: true);
-			roles.Add(partyGoers);
+        protected override void CreatePartyRoles()
+        {
+            var partyGoers = new LordPawnRole("PartyGoers", this) {
+                pawnValidator = (Pawn p) => true,
+                pawnReplenishPriority = (Pawn p) => 1f,
+                replenishCompleter = (List<Pawn> pawns) => false    //hungry, will default to everything
+            };
+            partyGoers.Configure(enabled: true, priority: 1, reassignableFrom: true
+                , seekReplacements: false, seekReplenishment: true);
+            roles.Add(partyGoers);
 
-			var snackMakers = new LordPawnRole("SnackMakers", this) {
-				pawnValidator = (Pawn p) => RecipeDefOf.CookMealSimple.PawnSatisfiesSkillRequirements(p),
-				pawnReplenishPriority = (Pawn p) => p.skills.GetSkill(SkillDefOf.Cooking).Level,
-				replenishCompleter = (List<Pawn> pawns) => pawns.Any()
+            var snackMakers = new LordPawnRole("SnackMakers", this) {
+                pawnValidator = (Pawn p) => RecipeDefOf.CookMealSimple.PawnSatisfiesSkillRequirements(p),
+                pawnReplenishPriority = (Pawn p) => p.skills.GetSkill(SkillDefOf.Cooking).Level,
+                replenishCompleter = (List<Pawn> pawns) => pawns.Any()
                     && pawns.Count >= (TotalSnacksNeeded() - SnacksAlreadySetup()) / 2
             };
             snackMakers.Configure(enabled: true, priority: 2, reassignableFrom: false
                 , seekReplacements: true, seekReplenishment: true);
-			roles.Add(snackMakers);
-		}
+            roles.Add(snackMakers);
+        }
 
-		override public bool AllowedToOrganize(Pawn pawn) =>
-			pawn.RaceProps.Humanlike && !pawn.InBed() && !pawn.InMentalState
-				&& pawn.GetLord() == null
-				&& pawn.AbleToStopJobForParty()
-				&& EnhancedPartyUtility.ShouldPawnKeepPartyingBasicChecks(pawn)
-				&& RecipeDefOf.CookMealSimple.PawnSatisfiesSkillRequirements(pawn);   
+        override public bool AllowedToOrganize(Pawn pawn) =>
+            pawn.RaceProps.Humanlike && !pawn.InBed() && !pawn.InMentalState
+                && pawn.GetLord() == null
+                && pawn.AbleToStopJobForParty()
+                && EnhancedPartyUtility.ShouldPawnKeepPartyingBasicChecks(pawn)
+                && RecipeDefOf.CookMealSimple.PawnSatisfiesSkillRequirements(pawn);   
 
-		override public bool PartyCanBeHadWith(Faction faction, Map map)
+        override public bool PartyCanBeHadWith(Faction faction, Map map)
         {
             bool value = PartyUtility.AcceptableGameConditionsToStartParty(map)
                         && map.regionGrid.allRooms.Any(room => room.Role == RoomRoleDefOf.RecRoom)
@@ -85,7 +85,7 @@ namespace EnhancedParty
                         && map.mapPawns.FreeColonistsSpawned.Count() >= def.minNumOfPartiers;
         }
 
-		public override bool TryGetOrganizerAndStartingSpot(Faction faction, Map map, out Pawn organizer, out IntVec3 startingSpot)
+        public override bool TryGetOrganizerAndStartingSpot(Faction faction, Map map, out Pawn organizer, out IntVec3 startingSpot)
         {
             var potentialOrganizers = map.mapPawns.SpawnedPawnsInFaction(faction)
                         .Where(pawn => AllowedToOrganize(pawn)).ToList();
@@ -117,25 +117,25 @@ namespace EnhancedParty
         
         public Room PartyRoom => PartySpot.GetRoom(this.lord.Map);
 
-		public bool IsInPartyArea(IntVec3 cell)
-		{
-			Map map = this.lord.Map;
-			return cell.GetRoom(map) == PartySpot.GetRoom(map);
-		}
+        public bool IsInPartyArea(IntVec3 cell)
+        {
+            Map map = this.lord.Map;
+            return cell.GetRoom(map) == PartySpot.GetRoom(map);
+        }
 
         override public void ExposeData()
         {
             base.ExposeData();
         }
 
-		public override float VoluntaryJoinPriorityFor(Pawn p)
-		{
-			float priority = base.VoluntaryJoinPriorityFor(p);
+        public override float VoluntaryJoinPriorityFor(Pawn p)
+        {
+            float priority = base.VoluntaryJoinPriorityFor(p);
 
-			if(EnhancedLordDebugSettings.logJoinPriorities)
-				Log.Message($"{this.GetType().Name} VoluntaryJoinPriorityFor Pawn: {p.Name} is {priority}");
+            if(EnhancedLordDebugSettings.logJoinPriorities)
+                Log.Message($"{this.GetType().Name} VoluntaryJoinPriorityFor Pawn: {p.Name} is {priority}");
 
-			return priority;
-		}
-	}
+            return priority;
+        }
+    }
 }
